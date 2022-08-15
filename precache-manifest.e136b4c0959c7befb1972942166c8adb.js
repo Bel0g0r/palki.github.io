ad15e1383de5e5a84ae4714a743ad71ee77e48d7
Bel0g0r <46893238+Bel0g0r@users.noreply.github.com>
2022-08-15 23:33:37 +0300

    Первая версия Палки времени VKApps

---
+++ precache-manifest.e136b4c0959c7befb1972942166c8adb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d086b2adf80a88bd5bf748ae700d7362",
+    "url": "/palki.github.io/index.html"
+  },
+  {
+    "revision": "9cc7935142f1bc5b2857",
+    "url": "/palki.github.io/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "8cc3009da01030e00601",
+    "url": "/palki.github.io/static/css/main.8222f150.chunk.css"
+  },
+  {
+    "revision": "9cc7935142f1bc5b2857",
+    "url": "/palki.github.io/static/js/2.40c45795.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/palki.github.io/static/js/2.40c45795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cc3009da01030e00601",
+    "url": "/palki.github.io/static/js/main.ededd379.chunk.js"
+  },
+  {
+    "revision": "0c28667f1ef37de0ba65",
+    "url": "/palki.github.io/static/js/runtime-main.6fa80142.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/palki.github.io/static/media/persik.4e1ec840.png"
+  }
+]);
